9dd3a1739d19aedcdc06ba96cc47ae8dc813a1a3
Christophe Rezk <christophe.rezk@gmail.com>
2016-09-09 23:07:06 +0000

    Setup Devise gem for user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Ttd::Application.configure do
+    config.action_mailer.default_url_options = { host: 'tdd-christophe-rezk.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
