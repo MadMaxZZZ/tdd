b9e4b9cd7d7a4ed6117f07530767ea2726c0da46
Christophe Rezk <christophe.rezk@gmail.com>
2016-09-09 22:51:47 +0000

    Setup the new form and tested

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,20 @@
 class GramsController < ApplicationController
+  def new
+    @gram = Gram.new
+  end
+
   def index
   end
+
+  def create
+    @gram = Gram.create(grams_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def grams_params
+    params.require(:gram).permit(:message)
+  end
+
 end
