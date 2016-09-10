cdfedb213065cb99cc81ea0e743ec803f8208617
Christophe Rezk <christophe.rezk@gmail.com>
2016-09-10 23:42:57 +0000

    Styling

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,9 @@
 class Gram < ActiveRecord::Base
-  mount_uploader :picture
   validates :message, presence: true
+  validates :picture, presence: true
+  has_many :comments
+
+    mount_uploader :picture, PictureUploader
+
   belongs_to :user
 end
